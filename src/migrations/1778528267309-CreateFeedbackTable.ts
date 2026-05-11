79b5f92662382bd5bd21895cc0e4b99a25e90bb9
arcuar <nunnaerim@gmail.com>
2026-05-12 04:39:49 +0900

    fix(#26): DB 마이그레이션 누락 파일 복구

---
+++ src/migrations/1778528267309-CreateFeedbackTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateFeedbackTable1778528267309 implements MigrationInterface {
+    name = 'CreateFeedbackTable1778528267309'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`feedbacks\` (\`id\` bigint UNSIGNED NOT NULL AUTO_INCREMENT, \`content\` varchar(500) NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`feedbacks\``);
+    }
+
+}
